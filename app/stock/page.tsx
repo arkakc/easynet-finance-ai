fd11e28cf7ed76399b17e414e8e4b2cbdd92f72c
helloeasynet386-a11y <helloeasynet386@gmail.com>
2026-09-07 00:08:05 +1000

    refactor: use multi-line purchase receipt workspace

--- app/stock/page.tsx
+++ app/stock/page.tsx
@@ -1,5 +1,5 @@
-import StockWorkspaceV2 from "@/app/components/stock-workspace-v2";
+import StockWorkspaceV3 from "@/app/components/stock-workspace-v3";
 
 export default function StockPage() {
-  return <StockWorkspaceV2 />;
+  return <StockWorkspaceV3 />;
 }
