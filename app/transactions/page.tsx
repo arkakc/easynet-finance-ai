6f5a9e5b1eaa24dc0b84a1fe7a1fdd68efd3e86d
helloeasynet386-a11y <helloeasynet386@gmail.com>
2026-09-07 20:38:35 +1000

    ux: enable edit actions in transaction list views

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,9 @@
 import TransactionsWorkspaceV4 from "@/app/components/transactions-workspace-v4";
+import TransactionListEditEnhancer from "@/app/components/transaction-list-edit-enhancer";
 
 export default function TransactionsPage() {
-  return <TransactionsWorkspaceV4 />;
+  return <>
+    <TransactionListEditEnhancer />
+    <TransactionsWorkspaceV4 />
+  </>;
 }
